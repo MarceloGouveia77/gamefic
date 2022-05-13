ee7f3bb00dc9f73b6db42de72110add6f0bf20d1
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2022-05-13 00:44:05 -0300

    styling login and registration

--- core/constants.py
+++ core/constants.py
@@ -1,122 +1,242 @@
 QUESTOES = [
-        {
-            "id": 1,
-            "questao": "Are you more comfortable, as a player on a MUD:",
-            "respostas": {
-                "Talking with friends in a tavern?": "S",
-                "Out hunting orcs by yourself for experience?": "A"
-            },
+    {
+        "id": 1,
+        "questao": "Qual é mais agradável para você?",
+        "respostas": {
+            "Matar um grande monstro": "A",
+            "Se gabando disso para seus amigos": "S"
         },
-        {
-            "id": 2,
-            "questao": "Which is more enjoyable to you?",
-            "respostas": {
-                "A Killing a big monster": "A",
-                "Bragging about it to your friends?": "S"
-            },
+    },
+    {
+        "id": 2,
+        "questao": "O que você mais gosta nas missões?",
+        "respostas": {
+            "Envolver-se no enredo": "S",
+            "Receber as recompensas no final": "A"
         },
-        {
-            "id": 3,
-            "questao": "Which do you enjoy more in MUD quests:",
-            "respostas": {
-                "Getting involved in the storyline": "S",
-                "Getting the rewards at the end?": "A"
-            },
+    },
+    {
+        "id": 3,
+        "questao": "Você prefere ser:",
+        "respostas": {
+            "Popular": "S",
+            "Rico": "A"
         },
-        {
-            "id": 4,
-            "questao": "Which would you rather be noticed for on a MUD?:",
-            "respostas": {
-                "Your equipment": "A",
-                "Your personality": "S"
-            },
+    },
+    {
+        "id": 4,
+        "questao": "O que você mais gosta em um jogo:",
+        "respostas": {
+            "Receber as últimas novidades": "S",
+            "Conseguir um novo item": "A"
         },
-        {
-            "id": 5,
-            "questao": "Would you rather be:",
-            "respostas": {
-                "Popular": "S",
-                "Wealthy": "A"
-            },
+    },
+    {
+        "id": 5,
+        "questao": "O que você preferiria ter:",
+        "respostas": {
+            "Um canal privado, através do qual você e seus amigos podem se comunicar": "S",
+            "Sua própria casa, que vale milhões de moedas de ouro": "A"
         },
-        {
-            "id": 6,
-            "questao": "Which do you enjoy more on a MUD?:",
-            "respostas": {
-                "Getting the latest gossip": "S",
-                "Getting a new item": "A"
-            },
+    },
+    {
+        "id": 6,
+        "questao": "O que você preferiria como jogador?",
+        "respostas": {
+            "Administrar sua própria taverna?": "S",
+            "Fazer seus próprios mapas do mundo e depois vendê-los?": "E"
         },
-        {
-            "id": 7,
-            "questao": "Which would you rather have, as a player on a MUD?:",
-            "respostas": {
-                "A private channel, over which you and your friends can communicate": "S",
-                "Your own house, worth millions of gold coins": "A"
-            },
+    },
+    {
+        "id": 7,
+        "questao": "O que é mais importante para você em um jogo online para você?",
+        "respostas": {
+            "A quantidade de pessoas": "S",
+            "A quantidade de áreas a explorar": "E"
         },
-        {
-            "id": 8,
-            "questao": "Which would you enjoy more as a MUD player?",
-            "respostas": {
-                "Running your own tavern?": "S",
-                "Making your own maps of the world, then selling them?": "E"
-            },
+    },
+    {
+        "id": 8,
+        "questao": "O que é mais importante para você:",
+        "respostas": {
+            "A qualidade do RPG em um jogo online": "S",
+            "A singularidade dos recursos e a mecânica do jogo": "E"
         },
-        {
-            "id": 9,
-            "questao": "What's more important in a MUD to you?",
-            "respostas": {
-                "The number of people": "S",
-                "The number of areas to explore": "E"
-            },
+    },
+    {
+        "id": 9,
+        "questao": "Você está sendo perseguido por um monstro, o que você faz?:",
+        "respostas": {
+            "Peça ajuda a um amigo para matá-lo": "S",
+            "Esconda-se em algum lugar que você sabe que o monstro não seguirá": "E"
         },
-        {
-            "id": 10,
-            "questao": "What's more important to you:",
-            "respostas": {
-                "The quality of roleplaying in a mud": "S",
-                "The uniqueness of the features, and game mechanic": "E"
-            },
+    },
+    {
+        "id": 10,
+        "questao": "Você é um jogador e quer lutar contra um dragão realmente forte. Como você abordaria esse problema?",
+        "respostas": {
+            "Consiga um grande grupo de jogadores para matá-lo": "S",
+            "Experimente uma variedade de armas e magia contra ele, até encontrar sua fraqueza.": "E"
         },
-        {
-            "id": 11,
-            "questao": "You are being chased by a monster on a MUD. Do you:",
-            "respostas": {
-                "Ask a friend for help in killing it": "S",
-                "Hide somewhere you know the monster won't follow": "E"
-            },
+    },
+    {
+        "id": 11,
+        "questao": "Você é morto por outro jogador. O que você faz?",
+        "respostas": {
+            "Descubra o porquê e tente convencê-los a não fazer isso de novo": "S",
+            "Planeja sua vingança": "K"
         },
-         {
-            "id": 12,
-            "questao": "You're a player on a mud, and you want to fight a really tough dragon. How would you approach this problem?",
-            "respostas": {
-                "Get a big group of players to kill it.": "S",
-                "Try a variety of weapons and magic against it, until you find its weakness.": "E"
-            },
-         },
-         {
-            "id": 13,
-            "questao": "You're a player on a mud, and about to go into an unknown dungeon. \nYou have your choice of one more person for your party. \nDo you bring:",
-            "respostas": {
-                "A bard, who's a good friend of yours and who's great for entertaining you and your friends": "S",
-                "A wizard, to identify the items that you find there": "E"
-            },
-         },
-         {
-            "id": 14,
-            "questao": "Is it better to be:",
-            "respostas": {
-                "Feared": "K",
-                "Loved": "S"
-            },
-         },
-         {
-            "id": 15,
-            "questao": "Someone has PK'ed you. Do you want to:",
-            "respostas": {
-                "Find out why, and try to convince them not to do it again": "S",
-                "Plot your revenge": "K"
-            },
-         }
-    ]
+    },
+    {
+        "id": 12,
+        "questao": "O que é mais emocionante?",
+        "respostas": {
+            "Um cenário bem interpretado": "S",
+            "Uma batalha mortal": "K"
+        },
+    },
+    {
+        "id": 13,
+        "questao": "Qual você gostaria mais?",
+        "respostas": {
+            "Vencer um duelo com outro jogador": "K",
+            "Ser aceito por um clã (um grupo de outros jogadores)": "S"
+        },
+    },
+    {
+        "id": 14,
+        "questao": "O que é pior?",
+        "respostas": {
+            "Ficar sem poderes": "K",
+            "Ficar sem amigos": "S"
+        },
+    },
+    {
+        "id": 15,
+        "questao": "Você preferiria:",
+        "respostas": {
+            "Ouvir o que alguém tem a dizer": "S",
+            "Mostre-lhes a lâmina afiada do seu machado": "K"
+        },
+    },
+    {
+        "id": 16,
+        "questao": "Em um jogo online, uma nova área se abre. O que você espera?:",
+        "respostas": {
+            "Explorar a nova área, descobrindo suas histórias": "E",
+            "Ser o primeiro a receber os novos equipamentos da área": "A"
+        },
+    },
+    {
+        "id": 17,
+        "questao": "Em um jogo online, você prefere ser conhecido como:",
+        "respostas": {
+            "Alguém que pode correr de quaisquer dois pontos do mundo e realmente sabe o caminho de volta": "E",
+            "A pessoa com o melhor e mais exclusivo equipamento do jogo": "A"
+        },
+    },
+    {
+        "id": 18,
+        "questao": "Você preferiria:",
+        "respostas": {
+            "Torne-se um herói mais rápido que seus amigos": "A",
+            "Conhecer mais segredos do que seus amigos": "E"
+        },
+    },
+    {
+        "id": 19,
+        "questao": "Você costuma:",
+        "respostas": {
+            "Saber coisas que ninguém mais sabe": "E",
+            "Tem itens que ninguém mais tem?": "A"
+        },
+    },
+    {
+        "id": 20,
+        "questao": "O que você prefere fazer?",
+        "respostas": {
+            "Resolver um enigma que ninguém mais resolveu": "E",
+            "Chegar a um certo nível de experiência mais rápido do que qualquer outra pessoa": "A"
+        },
+    },
+    {
+        "id": 21,
+        "questao": "Em um jogo online, preferiria ser conhecido por?",
+        "respostas": {
+            "Conhecimento": "E",
+            "Poder": "K"
+        },
+    },
+    {
+        "id": 22,
+        "questao": "Você prefere ganhar:",
+        "respostas": {
+            "Um concurso de curiosidades": "E",
+            "Uma batalha de arena": "K"
+        },
+    },
+    {
+        "id": 23,
+        "questao": "Se você está sozinho em uma área, você acha:",
+        "respostas": {
+            "É seguro explorar": "E",
+            "Você terá que procurar em outro lugar por presas": "K"
+        },
+    },
+    {
+        "id": 24,
+        "questao": "Você descobre que outro jogador está planejando sua morte. Você:",
+        "respostas": {
+            "Vai para uma área que seu oponente não conhece e prepare-se lá": "E",
+            "Ataca antes que ele ataque você": "K"
+        },
+    },
+    {
+        "id": 25,
+        "questao": "Você conhece um novo jogador. Você o considera como:",
+        "respostas": {
+            "Alguém que possa apreciar o seu conhecimento do jogo": "E",
+            "Como presa em potencial": "K"
+        },
+    },
+    {
+        "id": 26,
+        "questao": "Em um jogo online, você prefere:",
+        "respostas": {
+            "Ter uma espada duas vezes mais poderosa que qualquer outra no jogo": "A",
+            "Ser a pessoa mais temida do jogo": "K"
+        },
+    },
+    {
+        "id": 27,
+        "questao": "Em um jogo online, você estaria mais propenso a se gabar de:",
+        "respostas": {
+            "Quantos outros jogadores você matou": "K",
+            "Seu equipamento": "A"
+        },
+    },
+    {
+        "id": 28,
+        "questao": "Você prefere ter:",
+        "respostas": {
+            "Um feitiço para danificar outros jogadores": "K",
+            "Um feitiço que aumenta a taxa na qual você ganha pontos de experiência": "A"
+        },
+    },
+    {
+        "id": 29,
+        "questao": "Você prefere receber como recompensa de missão:",
+        "respostas": {
+            "Pontos de experiência": "A",
+            "Uma varinha com 3 cargas de um feitiço que permite controlar outros jogadores, contra a vontade deles": "K"
+        },
+    },
+    {
+        "id": 30,
+        "questao": "Ao jogar um videogame, é mais divertido:",
+        "respostas": {
+            "Ter a maior pontuação da lista": "A",
+            "Vencer o seu melhor amigo no um contra um": "K"
+        },
+    },
+]
